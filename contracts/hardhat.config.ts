0ce695607744ba90fc7d6529581e58be11a09f24
Andrew Liu <andrewjliu22@gmail.com>
2026-03-29 23:10:17 +0200

    contracts: Hardhat setup and package config

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,36 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "cancun",
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_BASE_MAINNET_RPC ?? "",
+        enabled: !!process.env.ALCHEMY_BASE_MAINNET_RPC,
+      },
+      chainId: 8453,
+    },
+    base: {
+      url: process.env.ALCHEMY_BASE_MAINNET_RPC ?? "",
+      chainId: 8453,
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [],
+    },
+  },
+};
+
+export default config;
